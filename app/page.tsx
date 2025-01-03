fdd4f638f16bd9f50bfb1145e420d582a15969a7
ray03j <eihouse.potato@outlook.jp>
2025-01-03 18:17:21 +0900

    feat: タイトルをダジャッジに変更

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>タイトル</h1>
+      <h1 className={styles.title}>ダジャッジ</h1>
       <div className={styles.inputContainer}>
         <input 
           type="text"
